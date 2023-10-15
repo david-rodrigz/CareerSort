cf6543769da834a5fa14beaec646f75961032ebc
David Rodriguez <davidrodriguez@u.boisestate.edu>
2023-10-15 16:25:15 -0600

    Organized pages and index file

---
+++ pages/jobs.php
@@ -0,0 +1,57 @@
+<section class="jobs-section">
+    <div class="job-filters">
+        <form action="" class="jobs-search-bar">
+            <input type="text" id="job" name="job" placeholder="search jobs or keywords">
+        </form>
+        <div class="filter">
+            <span>Location</span>
+            <div class="filter-content">
+                Filters go here
+            </div>
+        </div>
+        <div class="filter">
+            <span>Date Posted</span>
+            <div class="filter-content">
+                Filters go here
+            </div>
+        </div>
+        <div class="filter">
+            <span>Type</span>
+            <div class="filter-content">
+                Filters go here
+            </div>
+        </div>
+        <div class="filter">
+            <span>Pay</span>
+            <div class="filter-content">
+                Filters go here
+            </div>
+        </div>
+        <div class="filter">
+            <span>Experience Level</span>
+            <div class="filter-content">
+                Filters go here
+            </div>
+        </div>
+        <div class="filter">
+            <span>Education</span>
+            <div class="filter-content">
+                Filters go here
+            </div>
+        </div>
+    </div>
+    <div class="jobs-result">
+        <div class="jobs-list">
+            <div>Job goes here.</div>
+            <div>Job goes here.</div>
+            <div>Job goes here.</div>
+        </div>
+        <div class="job-post-container">
+            <div class="job-post">
+                <h1>Job title goes here</h1>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore itaque quibusdam adipisci provident corporis dolorum quis pariatur reiciendis odit cum dolore in architecto delectus accusamus quam, voluptatum et. Labore, corrupti!</p>
+            </div>
+        </div>
+    </div>
+</section>
