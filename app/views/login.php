f6d962a366d9399eccd292f05d0668b652be089a
David Rodriguez <davidrodriguez@u.boisestate.edu>
2023-12-04 13:19:05 -0700

    Updated CSS

--- app/views/login.php
+++ app/views/login.php
@@ -17,23 +17,27 @@ if (isset($password)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/admission.css">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <title>Log In</title>
 </head>
 <body>
-    <h1>Log In</h1>
-    <?php if (isset($error)): ?>
-        <p style="color: red;"><?= $error ?></p>
-    <?php endif; ?>
-    <form method="post">
-        <input id="text" type="text" name="username" placeholder="username"
-            <?php echo $username_value;?>>
-        <br><br>
-        <input id="text" type="password" name="password" placeholder="password"
-            <?php echo $password_value;?>>
-        <br><br>
-        <input id="button" type="submit" value="Login"><br><br>
-
-        <a href="/signup">Create a Free Account</a>
-    </form>
+    <div class="admission-container">
+        <h1>Log In</h1>
+        <?php if (isset($error)): ?>
+            <p style="color: red;"><?= $error ?></p>
+        <?php endif; ?>
+        <form method="post">
+            <input class="admission-prompt" id="text" type="text" name="username" placeholder="username"
+                <?php echo $username_value;?>>
+            <br><br>
+            <input class="admission-prompt" id="text" type="password" name="password" placeholder="password"
+                <?php echo $password_value;?>>
+            <br><br>
+            <input class="admission-btn" id="button" type="submit" value="Login"><br>
+    
+            <a href="/signup">Create a Free Account</a>
+        </form>
+    </div>
 </body>
 </html>
